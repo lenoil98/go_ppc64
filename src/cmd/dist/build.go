7ebcf0e4116932f64327bf92bb58697924ac478b
MikaelUrankar <mikael.urankar@gmail.com>
2022-05-17 16:16:52 +0200

    Add FreeBSD riscv64 support

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -1566,6 +1566,7 @@ var cgoEnabled = map[string]bool{
 	"freebsd/amd64":   true,
 	"freebsd/arm":     true,
 	"freebsd/arm64":   true,
+	"freebsd/riscv64": true,
 	"illumos/amd64":   true,
 	"linux/386":       true,
 	"linux/amd64":     true,
